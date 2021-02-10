6bdf059914421280d8a1b9371f9f4f1786cb6dc6
vivac0209 <71447545+vivac0209@users.noreply.github.com>
2021-02-10 18:44:25 +0800

    Update while2.py

--- while2.py
+++ while2.py
@@ -1,16 +1,17 @@
 x=3
 password ='a123456'
-while True:
+while x >0:
+	x=x-1
 	key = input('請輸入密碼')
-	if key != password:
-		x=x-1;
-		print('密碼錯誤! 還有' , x ,'次機會');
-		if x==0:
-			print('end 最多輸入三次');
-			break;
-	elif key == 'a123456':
+	if key == 'a123456':
 		print('登入成功')
-		break;
+		break
+	else:
+		print('密碼錯誤!')
+		if x > 0:
+			print('還有', x ,'次機會')
+		else:
+			print('end')
 
 
 
